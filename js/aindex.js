87cc087ea8653d87d069460eba79f478e7b65446
booldook <booldook@gmail.com>
2020-06-03 13:19:29 +0900

    booldook

--- js/aindex.js
+++ js/aindex.js
@@ -1,11 +1,26 @@
 
-
-
-if(scTop > 800) $(".bt-top").css("visivility", "visible");
-else $(".bt-top").css("visivility", "hidden");
+function onScroll() {
+	var scTop = $(this).scrollTop();
+	if(scTop > 800) $(".bt-top").css("visibility", "visible");
+	else $(".bt-top").css("visibility", "hidden");
+}
 
 function onTopClick() {
   $("html, body").stop().animate({"scrollTop": 0}, 800);
 }
 
+function onHover() {
+	$(this).find(".subs").stop().fadeIn(500);s
+}
+
+function onLeave() {
+	$(this).find(".subs").stop().fadeOut(500);
+}
+
+
 $(".bt-top").click(onTopClick);
+
+$(window).scroll(onScroll);
+
+$(".header .navi").hover(onHover, onLeave);
+
